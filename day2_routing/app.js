4da67403f9ca6e23b065d94f4526e3437cd2049c
Rushabh Halmare <42460595+rishabhhalmare07@users.noreply.github.com>
2021-08-03 09:02:40 +0530

    routing

---
+++ day2_routing/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req,res) => {
+    res.send("Welcome to Home Page");
+});
+
+app.get("/about", (req,res) => {
+    // using 200 means client (search on web)
+    //res.status(200).send("Welcome to AboutUs Page");
+    res.send("Welcome to AboutUs Page");
+});
+
+app.get("/contac", (req,res) => {
+    res.send("Welcome to contact Page");
+});
+
+app.get("/temp", (req,res) => {
+    res.send("Welcome to temp Page");
+});
+
+app.listen(port, () => {
+    // `` using this is a ES6 way
+    console.log(`Listening to port no. ${port}`);
+});
